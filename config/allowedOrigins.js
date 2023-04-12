439b3d215dfe70c3396a1acfda27df81922d2902
Zuhair <zuhairahmed055@gmail.com>
2023-04-12 23:03:52 +0530

    4th commit : updated allowed orgins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3500", "http://127.0.0.1:5173"];
+const allowedOrigins = [
+  "http://localhost:3500",
+  "http://127.0.0.1:5173",
+  "https://e-commune.vercel.app",
+];
 
 module.exports = allowedOrigins;
