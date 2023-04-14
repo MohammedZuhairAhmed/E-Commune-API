1e506d14b982a41df401c9dfa91b0cd46686e00b
Zuhair <zuhairahmed055@gmail.com>
2023-04-14 23:19:26 +0530

    updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:5173",
-  "https://e-commune-api.onrender.com",
+  "https://e-commune.onrender.com",
 ];
 
 module.exports = allowedOrigins;
