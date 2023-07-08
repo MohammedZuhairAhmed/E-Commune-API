5c7a0229c09b8f05ff1cc34c0f03154cc4867ced
Zuhair <zuhairahmed055@gmail.com>
2023-07-08 10:10:56 +0530

    added build address

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:5173",
+  "http://127.0.0.1:4173",
+  "http://localhost:4173",
   "https://e-commune.onrender.com/",
   "https://e-commune.onrender.com",
 ];
